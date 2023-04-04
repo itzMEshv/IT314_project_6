4d6271f6f228600fa6896b62178e28e420d16212
prashr1 <shroffpraneet@gmail.com>
2023-04-04 22:42:54 +0530

    Login frontend added

---
+++ Auto Attendance System/views/login/js/instructorLogin.js
@@ -0,0 +1,21 @@
+let showPasssword = true;
+
+function togglePassword(){
+const eyeIcon = document.getElementById("eyeIcon");
+const password = document.getElementById("signInPassword");
+
+if(showPasssword){
+    console.log("showPasswordtrue");
+    password.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+    showPasssword = false;
+}
+else {
+    console.log("showPasswordtrue");
+    password.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+    showPasssword = true;
+}
+}
