d70382952677bc3f80c5eafc1934393bc88dd27e
Poojan Patel <poojanpatel02112002@gmail.com>
2023-04-04 13:50:14 +0530

    Added passport configuration for automatic login and logout functionality

--- Auto Attendance System/app.js
+++ Auto Attendance System/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose")
+const passport = require("passport");
+const {initializingPassport,isAuthenticated} = require("./passportConfig");
 const expressSession = require("express-session")
 const path = require("path")
 const User = require("./models/user");
@@ -13,8 +15,39 @@ const con = mongoose.connection
 con.on('open',()=>{
     console.log('Database connected...')
 })
+
+initializingPassport(passport);
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use(expressSession({secret:"secret",resave:false,
+saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("view engine","ejs")
+
+//static files
+app.use('/homePage',express.static(__dirname + '/views/homePage'));
+
+
+
+// home page
+app.get("/",async(req,res)=>{
+    if(!req.user) res.render("homePage/homePage");
+    else{
+        if(req.user.role === "student")res.redirect("/dashboard/student");
+        else res.redirect("/dashboard/instructor");
+    }
+});
+
+
+
+// register student
+app.get("/register/student",(req,res)=>{
+    res.render("register/studentRegister")
+})
 app.post("/register/student",async(req,res)=>{
     const user = await User.findOne({email:req.body.email})
     if(user){
@@ -26,51 +59,25 @@ app.post("/register/student",async(req,res)=>{
     }
 });
 
-//static files
 
-app.use('/homePage',express.static(__dirname + '/views/homePage'));
+// login student
+app.post("/login/student",passport.authenticate("local",{failureRedirect:"/login/student",successRedirect:"/dashboard/student"}),(req,res)=>{
 
-
-app.post("/login/student",async(req,res)=>{
-    const usr = await user.findOne({email:req.body.email,password:req.body.password})
-    if(usr){
-        if(usr.role == "instructor"){
-            res.redirect("/login/student")
-        }
-        else{
-            res.send("Student Dashboard")
-        }
-    }else{
-        res.redirect("/login/student")
-    }
 })
 app.get("/login/student",(req,res)=>{
     res.render("login/studentLogin");
 })
 
-app.set("view engine","ejs")
-
-app.get("/",async(req,res)=>{
-    res.render("homePage/homePage");
-});
-
-app.get("/register/student",(req,res)=>{
-    res.render("register/studentRegister")
-})
 
 // instructor login
-app.post("/login/instructor",async(req,res)=>{
-    const user = await User.findOne({email:req.body.email,role:"instructor"});
-    if(!user){
-        res.redirect("/login/instructor");
-    }
-    else{
-        res.redirect("/dashboard/instructor");
-    }
+app.post("/login/instructor",passport.authenticate("local",{failureRedirect:"/login/instructor",successRedirect:"/dashboard/instructor"}),(req,res)=>{
+    
 })
 app.get("/login/instructor",(req,res)=>{
     res.render("login/instructorLogin");
 })
+
+
 
 // register new instructor
 app.get("/register/instructor",(req,res)=>{
@@ -87,10 +94,42 @@ app.post("/register/instructor",async(req,res)=>{
     }
 })
 
+
+
 // instructor dashboard
-app.get("/dashboard/instructor",(req,res)=>{
-    res.send("This is instructor dashboard")
+app.get("/dashboard/instructor",async(req,res)=>{
+    if(!req.user)res.redirect("/");
+    else{
+        if(req.user.role == "student"){
+            res.redirect("/dashboard/student");
+        }
+        else{
+            res.render("dashboard/instructorDashboard");
+        }
+    }
 })
+
+
+// student dashboard
+app.get("/dashboard/student",async(req,res)=>{
+    if(!req.user)res.redirect("/");
+    else{
+        if(req.user.role == "instructor"){
+            res.redirect("/dashboard/instructor");
+        }
+        else{
+            res.render("dashboard/studentDashboard");
+        }
+    }
+})
+
+// logout
+app.get("/logout",(req,res,next)=>{
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000")
---
+++ Auto Attendance System/passportConfig.js
@@ -0,0 +1,39 @@
+const LocalStrategy = require("passport-local").Strategy
+const User = require("./models/user");
+exports.initializingPassport = (passport)=>{
+
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },async(req,username,password,done)=>{
+        try{
+            const user = await User.findOne({email:username});
+            if (!user)return done(null,false);
+            if(user.password !== password || req.body.role !== user.role)return done(null,false);
+            else return done(null,user);
+        }
+        catch(err){
+            return done(err,false);
+        }
+    }))
+
+    passport.serializeUser((user,done)=>{
+        done(null,user.id);
+    });
+
+    passport.deserializeUser(async(id,done)=>{
+        try{
+            const user = await User.findById(id);
+            done(null,user);
+        }
+        catch(err){
+            done(err,false);
+        }
+    });
+
+};
+exports.isAuthenticated = (req,res,next)=>{
+    if(req.user)return next();
+    res.redirect("/");
+}
